fe3f54130e2ef75c987ecc7c98c69351284088d3
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-15 11:49:22 +0300

    changes color of heading

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -8,7 +8,7 @@ const Main = () => {
     <main>
         <section className='first-section'>
             <div className="first-section-left">
-                <h1>A simple Bookmark Manager</h1>
+                <h1>A simple Bookmark <span>Manager</span></h1>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laboriosam dignissimos ipsam repellat rerum vel necessitatibus pariatur vero nisi fuga provident, dolorem impedit iure molestiae, libero, cumque omnis. Laborum, quos.</p>
                 <div className="first-section-inner-container">
                     <Button buttonText={"Get it on Chrome"}/>
