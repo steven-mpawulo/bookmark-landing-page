a7e99d54c778deb28e54ec5ff42027df9a9853dd
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-16 13:35:29 +0300

    modifies question container to fill the entire width

--- src/components/common/question/Question.js
+++ src/components/common/question/Question.js
@@ -2,7 +2,7 @@ import React from 'react'
 import './question.css'
 const Question = ({question, ArrowIcon}) => {
     return (
-        <div>
+        <div className='wrapper'>
             <div className="question-container">
                 <p>{question}?</p>
                 <img src={ArrowIcon} alt="arrow-down" className='arrow-down' />
