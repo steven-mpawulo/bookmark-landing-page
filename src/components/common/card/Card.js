360a9e91f30cd4981964343946d884afbf709fea
steven-mpawulo <stevenmpawulo@gmail.com>
2023-08-16 11:21:42 +0300

    finishes up on html of third section

--- src/components/common/card/Card.js
+++ src/components/common/card/Card.js
@@ -1,16 +1,16 @@
 import React from 'react'
 import Button from '../button/Button'
 
-const Card = ({icon, browserName, browserVersion}) => {
-  return (
-    <div className="card">
-        <img src={icon} alt="icon" />
-        <p>Add to {browserName}</p>
-        <p>Minimum version {browserVersion}</p>
-        <hr />
-        <Button buttonText={"Add & Install Extension"} backgroundColor={"hsl(231, 69%, 60%)"} color={"white"} />
-    </div>
-  )
+const Card = ({ icon, browserName, browserVersion }) => {
+    return (
+        <div className="card">
+            <img src={icon} alt="icon" />
+            <p>Add to {browserName}</p>
+            <p>Minimum version {browserVersion}</p>
+            <hr />
+            <Button buttonText={"Add & Install Extension"} backgroundColor={"hsl(231, 69%, 60%)"} color={"white"} />
+        </div>
+    )
 }
 
 export default Card
--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -2,58 +2,63 @@ import React from 'react'
 import Button from '../common/button/Button'
 import IllustrationHero from '../../images/illustration-hero.svg'
 import './main.css'
+import Card from '../common/card/Card'
+import ChromeIcon from '../../images/logo-chrome.svg'
 
 const Main = () => {
-  return (
-    <main>
-        <section className='first-section'>
-            <div className="first-section-left">
-                <h1>A simple Bookmark <span>Manager</span></h1>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laboriosam dignissimos ipsam repellat rerum vel necessitatibus pariatur vero nisi fuga provident, dolorem impedit iure molestiae, libero, cumque omnis. Laborum, quos.</p>
-                <div className="first-section-inner-container">
-                    <Button buttonText={"Get it on Chrome"} backgroundColor={"hsl(231, 69%, 60%)"} color={"white"} marginRight={"20px"}/>
-                    <Button buttonText={"Get it on Firefox"}/>
+    return (
+        <main>
+            <section className='first-section'>
+                <div className="first-section-left">
+                    <h1>A simple Bookmark <span>Manager</span></h1>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam laboriosam dignissimos ipsam repellat rerum vel necessitatibus pariatur vero nisi fuga provident, dolorem impedit iure molestiae, libero, cumque omnis. Laborum, quos.</p>
+                    <div className="first-section-inner-container">
+                        <Button buttonText={"Get it on Chrome"} backgroundColor={"hsl(231, 69%, 60%)"} color={"white"} marginRight={"20px"} />
+                        <Button buttonText={"Get it on Firefox"} />
+                    </div>
                 </div>
-            </div>
-            <div className="first-section-right">
-                <div className="background-container">
+                <div className="first-section-right">
+                    <div className="background-container">
+                    </div>
+                    <div className="image-container">
+                        <img src={IllustrationHero} alt="illustration-hero" className='illustration-hero' />
+                    </div>
+
                 </div>
-                <div className="image-container">
-                <img src={IllustrationHero} alt="illustration-hero" className='illustration-hero'/>
+            </section>
+            <section className='second-section'>
+                <h2>Features</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur amet eligendi fugiat adipisci dolor at, quaerat nobis fugit assumenda aliquid quos pariatur necessitatibus consequatur dolore accusantium qui officia non quisquam!</p>
+                <div className="container-for-tab-section">
+                    <span>Simple Bookmarking</span>
+                    <span>Speedy Searching</span>
+                    <span>Easy Sharing</span>
                 </div>
-                
-            </div>
-        </section>
-        <section className='second-section'>
-            <h2>Features</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequuntur amet eligendi fugiat adipisci dolor at, quaerat nobis fugit assumenda aliquid quos pariatur necessitatibus consequatur dolore accusantium qui officia non quisquam!</p>
-            <div className="container-for-tab-section">
-                <span>Simple Bookmarking</span>
-                <span>Speedy Searching</span>
-                <span>Easy Sharing</span>
-            </div>
-            <div className="second-section-inner-container">
-                <div className="left-inner-container">
-                <div className="container-for-background"></div>
-                <div className="container-for-image">
-                    <img src={IllustrationHero} alt="illustration-hero-2" className='illustration-hero-2' />
+                <div className="second-section-inner-container">
+                    <div className="left-inner-container">
+                        <div className="container-for-background"></div>
+                        <div className="container-for-image">
+                            <img src={IllustrationHero} alt="illustration-hero-2" className='illustration-hero-2' />
+                        </div>
+
+                    </div>
+
+                    <div className="right-inner-container">
+                        <h2>Bookmark in one click</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam animi dolorem sunt ut, at labore praesentium eum alias consequuntur voluptatibus placeat soluta quo officia perferendis aliquam ea voluptatem amet dolore?</p>
+                        <Button buttonText={"More Info"} backgroundColor={"hsl(231, 69%, 60%)"} color={"white"} />
+                    </div>
                 </div>
-                
+            </section>
+            <section className='third-section'>
+                <h2>Download the extension</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aperiam sed ex nobis. Aliquid deserunt id autem nostrum illum culpa ipsam maiores quos earum reprehenderit et, facere iste porro consequatur.</p>
+                <div className="container-for-cards">
+                    <Card browserName={"Chrome"} browserVersion={42} icon={ChromeIcon} />
                 </div>
-                
-                <div className="right-inner-container">
-                    <h2>Bookmark in one click</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam animi dolorem sunt ut, at labore praesentium eum alias consequuntur voluptatibus placeat soluta quo officia perferendis aliquam ea voluptatem amet dolore?</p>
-                    <Button buttonText={"More Info"} backgroundColor={"hsl(231, 69%, 60%)"} color={"white"} />
-                </div>
-            </div>
-        </section>
-        <section className='third-section'>
-            <h2>Download the extension</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam aperiam sed ex nobis. Aliquid deserunt id autem nostrum illum culpa ipsam maiores quos earum reprehenderit et, facere iste porro consequatur.</p>
-        </section>
-    </main>
-  )
+            </section>
+        </main>
+    )
 }
 
 export default Main
